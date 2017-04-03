d58d43b2d7579edf38fcdebc23ed8a01a7985264
Kevin <kchannan75@gmail.com>
2017-04-03 18:14:02 -0400

    Added CheckerType enum and constuctor to Checkers class, added constructor to Board class

--- Checkers/src/checkers/Board.java
+++ Checkers/src/checkers/Board.java
@@ -1,5 +1,21 @@
 package checkers;
 
-public class Board {
+import java.awt.GridLayout;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Board extends JFrame {
+	
+	//private GridLayout boardLayout = new GridLayout(0,8,0,0);
+	JPanel theBoard = new JPanel();
+
+	
+	public Board(){
+		this.setTitle("Checkers");
+		
+	}
+	
+	
+	
 }
--- Checkers/src/checkers/Checker.java
+++ Checkers/src/checkers/Checker.java
@@ -1,6 +1,42 @@
 package checkers;
 
-public class Checker {
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+
+import checkers.Checker.CheckerType;
+
+public class Checker extends JButton {
+	public enum CheckerType {
+		RED, RED_KING, BLACK, BLACK_KING;
+	}
+	
+	int x; 
+	int y;
+	
+	
+	
+	
+	public CheckerType checkerType;
+	public Checker(CheckerType checkerType) {
+		this.checkerType = checkerType;
+		switch (checkerType) {
+		case RED:
+			this.setBackground(Color.RED);
+			
+			break;
+		case BLACK:
+			this.setBackground(Color.BLACK);
+		
+			break;
+		}
+		
+		this.setSize(100, 100);
+	}
+	
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
