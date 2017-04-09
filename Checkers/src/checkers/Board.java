bdc45172e0626f134662f6c144e08fb82728d1b9
Kevin <kchannan75@gmail.com>
2017-04-09 10:27:53 -0400

    Added turn switching, fixed a bug where buttons from previous turns were disappearing, need to make it so that previously moved pieces can be moved again.

--- Checkers/src/checkers/Board.java
+++ Checkers/src/checkers/Board.java
@@ -22,6 +22,9 @@ public class Board extends JPanel implements ActionListener {
 
 	public Board() {
 		setLayout(new GridBagLayout());
+		theBlank.setBackground(Color.lightGray);
+		theRed.setBackground(Color.lightGray);
+		theBlack.setBackground(Color.lightGray);
 		for (int row = 0; row < 8; row++) {
 			for (int col = 0; col < 8; col++) {
 				if ((row + col) % 2 == 0) {
@@ -54,75 +57,148 @@ public class Board extends JPanel implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 		Checker checkerPressed = (Checker) e.getSource();
-
+		Checker theBlank = new Checker(CheckerType.BLANK);
+		Checker theRed = new Checker(CheckerType.RED);
+		Checker theBlack = new Checker(CheckerType.BLACK);
+		theBlank.addActionListener(this);
+		theRed.addActionListener(this);
+		theBlack.addActionListener(this);
 		if (whoseTurn == 0) {
 			if (turnInProgress == false) {
 
 				if (checkerPressed.isBlack()) {
 
-					checkerPressed.setBackground(Color.yellow);
+					checkerPressed.setActivePiece();
 					temp = checkerPressed;
 					turnInProgress = true;
 				} else {
 					if (checkerPressed.isRed() || checkerPressed.isBlank()) {
-						System.out.print("It's Black's turn, please select a Black piece to move.");
+						System.out.println("It's Black's turn, please select a Black piece to move.");
+
 					}
 
 				}
 			} else {
 				if (checkerPressed.isBlack() || checkerPressed.isRed()) {
-					System.out.print("That's not a legal move, silly!");
+					System.out.println("Pressed other piece");
+					temp.unactivatePiece();
+					turnInProgress = false;
 				} else {
 					if (checkerPressed.getYPosition() == temp.getYPosition() - 1) {
 						if ((checkerPressed.getXPosition() == temp.getXPosition() - 1)
 								|| (checkerPressed.getXPosition() == temp.getXPosition() + 1)) {
-							
+
 							Checker movedTo = checkerSet[checkerPressed.getYPosition()][checkerPressed.getXPosition()];
 							Checker movedFrom = checkerSet[temp.getYPosition()][temp.getXPosition()];
+							// checkerSet[checkerPressed.getYPosition()][checkerPressed.getXPosition()]
+							// = new Checker(CheckerType.BLACK);
+							// checkerSet[temp.getYPosition()][temp.getXPosition()]
+							// = new Checker(CheckerType.BLANK);
+
 							theBlank.setBackground(Color.lightGray);
-							
-							GridBagLayout layout = (GridBagLayout)getLayout();
+
+							GridBagLayout layout = (GridBagLayout) getLayout();
 							GridBagConstraints gbc = layout.getConstraints(movedTo);
+							
 							remove(movedTo);
-							add(new Checker(CheckerType.BLACK), gbc);
+							
+							add(theBlack, gbc);
+
+							gbc = layout.getConstraints(movedFrom);
+							remove(movedFrom);
+							add(theBlank, gbc);
 							revalidate();
 							repaint();
-						    gbc = layout.getConstraints(movedFrom);
-						    remove(movedFrom);
-						    add(theBlank, gbc);
-						 	revalidate();
-							repaint();
-						    
-							
-							
-							
-						//	checkerSet[checkerPressed.getYPosition()][checkerPressed.getXPosition()] = temp; 
-							
-							
-						
-							
-							System.out.print("Legal move");
+							checkerSet[temp.getYPosition()][temp.getXPosition()].addActionListener(this);
+							checkerSet[checkerPressed.getYPosition()][checkerPressed.getXPosition()].addActionListener(this);
+
+							System.out.println("Legal move");
 							turnInProgress = false;
 							whoseTurn = 1;
 						} else
-							System.out.print("Not a legal move!");
+							System.out.println("Not a legal move! Wrong X");
+						temp.unactivatePiece();
+						turnInProgress = false;
+
 					} else {
-						System.out.print("Not a legal move!");
+						System.out.println("Not a legal move! Wrong Y");
+						temp.unactivatePiece();
+						turnInProgress = false;
 					}
 
 				}
 
 			}
-			/*
-			 * if(temp.isRed()) {
-			 * System.out.print("You clicked a red at X = "+temp.getXPosition()
-			 * +", Y = "+ temp.getYPosition()); } else if(temp.isBlack()) {
-			 * System.out.print("You clicked a black"); } else if
-			 * (temp.isBlank()){ System.out.print("You clicked a blank"); }
-			 */
 
+		} else if (whoseTurn == 1) {
+			if (turnInProgress == false) {
+
+				if (checkerPressed.isRed()) {
+
+					checkerPressed.setActivePiece();
+					temp = checkerPressed;
+					turnInProgress = true;
+				} else {
+					if (checkerPressed.isBlack() || checkerPressed.isBlank()) {
+						System.out.println("It's Black's turn, please select a Black piece to move.");
+
+					}
+
+				}
+			} else {
+				if (checkerPressed.isBlack() || checkerPressed.isRed()) {
+					System.out.println("Not a valid Move!");
+					temp.unactivatePiece();
+					turnInProgress = false;
+				} else {
+					if (checkerPressed.getYPosition() == temp.getYPosition() + 1) {
+						if ((checkerPressed.getXPosition() == temp.getXPosition() - 1)
+								|| (checkerPressed.getXPosition() == temp.getXPosition() + 1)) {
+
+							Checker movedTo = checkerSet[checkerPressed.getYPosition()][checkerPressed.getXPosition()];
+							Checker movedFrom = checkerSet[temp.getYPosition()][temp.getXPosition()];
+							// checkerSet[checkerPressed.getYPosition()][checkerPressed.getXPosition()]
+							// = new Checker(CheckerType.RED);
+							// checkerSet[temp.getYPosition()][temp.getXPosition()]
+							// = new Checker(CheckerType.BLANK);
+							
+							System.out.println(checkerSet[checkerPressed.getYPosition()][checkerPressed.getXPosition()].getXPosition());
+							System.out.println(checkerSet[temp.getYPosition()][temp.getXPosition()].getXPosition());
+							theBlank.setBackground(Color.lightGray);
+
+							GridBagLayout layout = (GridBagLayout) getLayout();
+							GridBagConstraints gbc = layout.getConstraints(movedTo);
+							remove(movedTo);
+							add(theRed, gbc);
+							revalidate();
+							repaint();
+
+							gbc = layout.getConstraints(movedFrom);
+							remove(movedFrom);
+							add(theBlank, gbc);
+							revalidate();
+							repaint();
+							checkerSet[temp.getYPosition()][temp.getXPosition()].addActionListener(this);
+							checkerSet[checkerPressed.getYPosition()][checkerPressed.getXPosition()].addActionListener(this);
+							System.out.println("Legal move");
+							turnInProgress = false;
+							whoseTurn = 0;
+						} else
+							System.out.println("Not a legal move! Try again.");
+						temp.unactivatePiece();
+						turnInProgress = false;
+
+					} else {
+						System.out.println("Not a legal move! Try again.");
+						temp.unactivatePiece();
+						turnInProgress = false;
+					}
+
+				}
+
+			}
 		}
 
 	}
-	
+
 }
--- Checkers/src/checkers/Checker.java
+++ Checkers/src/checkers/Checker.java
@@ -14,79 +14,81 @@ public class Checker extends JButton {
 	public enum CheckerType {
 		RED, RED_KING, BLACK, BLACK_KING, BLANK;
 	}
-	
-	
+
 	private String color;
 	private int x;
 	private int y;
-	
-	
-	public boolean isRed()
-	{
+
+	public boolean isRed() {
 		if (this.color == "Red")
 			return true;
 		else
 			return false;
-		
+
 	}
-	public boolean isBlack()
-	{
+
+	public boolean isBlack() {
 		if (this.color == "Black")
 			return true;
 		else
 			return false;
 	}
-	public boolean isBlank()
-	{
+
+	public boolean isBlank() {
 		if (this.color == "Blank")
 			return true;
 		else
 			return false;
 	}
-	public int getXPosition(){
+
+	public int getXPosition() {
 		return this.x;
 	}
-	
-	public int getYPosition(){
+
+	public int getYPosition() {
 		return this.y;
 	}
-	public void setXY(int x, int y)
-	{
+
+	public void setXY(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
-	
-	
-	
-	public CheckerType checkerType;
-	
-	public Checker(){
-		
+
+	public void setActivePiece() {
+		this.setBackground(Color.yellow);
 	}
-	
-	
+
+	public void unactivatePiece() {
+		this.setBackground(Color.lightGray);
+	}
+
+	public CheckerType checkerType;
+
+	public Checker() {
+
+	}
+
 	public Checker(CheckerType checkerType) {
 		this.checkerType = checkerType;
 		switch (checkerType) {
 		case RED:
 			this.setIcon(RedImg);
 			this.color = "Red";
+			this.setBackground(Color.lightGray);
 			break;
 		case BLACK:
 			this.setIcon(BlackImg);
 			this.color = "Black";
+			this.setBackground(Color.lightGray);
 			break;
 		case BLANK:
 			this.color = "Blank";
+			this.setBackground(Color.lightGray);
 			break;
 		}
-		
-		
 
 		setPreferredSize(new Dimension(50, 50));
 	}
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
